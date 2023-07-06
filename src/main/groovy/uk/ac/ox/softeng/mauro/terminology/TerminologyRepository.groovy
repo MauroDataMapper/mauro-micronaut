51774a83b5a6f8ca0f6bb5aa7a553dc1eaf31557
Joe Crawford <joseph.crawford@bdi.ox.ac.uk>
2023-07-06 17:13:24 +0100

    WIP

--- src/main/groovy/uk/ac/ox/softeng/mauro/terminology/TerminologyRepository.groovy
+++ src/main/groovy/uk/ac/ox/softeng/mauro/terminology/TerminologyRepository.groovy
@@ -11,6 +11,14 @@ import reactor.core.publisher.Mono
 @R2dbcRepository(dialect = Dialect.POSTGRES)
 abstract class TerminologyRepository implements ReactorPageableRepository<Terminology, UUID>, ModelRepository<Terminology> {
 
+
+    /* todo: use sql:
+    SELECT terminology.*, term.*, term_relationship.*, term_relationship_type.*
+FROM terminology
+LEFT JOIN term ON terminology.id = term.terminology_id
+LEFT JOIN term_relationship ON term.id IN (source_term_id, target_term_id)
+LEFT JOIN term_relationship_type ON terminology.id = term_relationship_type.terminology_id;
+     */
     @Join(value = 'terms', type = Join.Type.LEFT_FETCH)
     @Join(value = 'termRelationshipTypes', type = Join.Type.LEFT_FETCH)
     @Join(value = 'termRelationships', type = Join.Type.LEFT_FETCH)
