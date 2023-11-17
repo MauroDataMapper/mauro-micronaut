beee6f6349eca769e79465d8bf139f667920ca0c
James Welch <james.welch@cs.ox.ac.uk>
2023-11-17 14:52:13 +0000

    Getting DataTypes and EnumerationValues working

--- mauro-persistence/src/main/groovy/uk/ac/ox/softeng/mauro/persistence/datamodel/DataTypeRepository.groovy
+++ mauro-persistence/src/main/groovy/uk/ac/ox/softeng/mauro/persistence/datamodel/DataTypeRepository.groovy
@@ -2,17 +2,20 @@ package uk.ac.ox.softeng.mauro.persistence.datamodel
 
 import uk.ac.ox.softeng.mauro.domain.datamodel.DataModel
 import uk.ac.ox.softeng.mauro.domain.datamodel.DataType
+
 import uk.ac.ox.softeng.mauro.domain.model.AdministeredItem
 import uk.ac.ox.softeng.mauro.persistence.model.ModelItemRepository
 
-import groovy.transform.CompileStatic
+import io.micronaut.core.annotation.NonNull
 import io.micronaut.data.model.query.builder.sql.Dialect
 import io.micronaut.data.r2dbc.annotation.R2dbcRepository
+
 import io.micronaut.data.repository.reactive.ReactorPageableRepository
+import jakarta.validation.Valid
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
-@CompileStatic
+
 @R2dbcRepository(dialect = Dialect.POSTGRES)
 abstract class DataTypeRepository implements ReactorPageableRepository<DataType, UUID>, ModelItemRepository<DataType> {
 
@@ -25,6 +28,8 @@ abstract class DataTypeRepository implements ReactorPageableRepository<DataType,
     abstract Mono<Long> deleteByDataModelId(UUID dataModelId)
 
     abstract Flux<DataType> readAllByDataModel(DataModel dataModel)
+
+    abstract Mono<DataType> save(@Valid @NonNull DataType item)
 
     @Override
     Mono<DataType> findByParentIdAndId(UUID parentId, UUID id) {
@@ -56,4 +61,5 @@ abstract class DataTypeRepository implements ReactorPageableRepository<DataType,
     Boolean handles(String domainType) {
         domainType.toLowerCase() in ['datatype', 'datatypes']
     }
+
 }
