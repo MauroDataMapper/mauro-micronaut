530e8b412d8f098dded661a840bfc37931ec9e3c
Joe Crawford <joseph.crawford@bdi.ox.ac.uk>
2024-03-05 15:52:03 +0000

    Add abstract FacetController

--- mauro-domain/src/main/groovy/uk/ac/ox/softeng/mauro/domain/facet/Metadata.groovy
+++ mauro-domain/src/main/groovy/uk/ac/ox/softeng/mauro/domain/facet/Metadata.groovy
@@ -1,25 +1,10 @@
 package uk.ac.ox.softeng.mauro.domain.facet
 
-import uk.ac.ox.softeng.mauro.domain.model.AdministeredItem
-
 import groovy.transform.AutoClone
 import groovy.transform.CompileStatic
-import io.micronaut.core.annotation.Introspected
-import io.micronaut.core.annotation.Nullable
-import io.micronaut.data.annotation.DateCreated
-import io.micronaut.data.annotation.DateUpdated
-import io.micronaut.data.annotation.GeneratedValue
-import io.micronaut.data.annotation.Id
 import io.micronaut.data.annotation.Index
 import io.micronaut.data.annotation.Indexes
 import io.micronaut.data.annotation.MappedEntity
-import io.micronaut.data.annotation.MappedProperty
-import io.micronaut.data.annotation.Relation
-import io.micronaut.data.annotation.TypeDef
-import io.micronaut.data.annotation.Version
-import io.micronaut.data.model.DataType
-
-import java.time.Instant
 
 @CompileStatic
 @MappedEntity(schema = 'core')
