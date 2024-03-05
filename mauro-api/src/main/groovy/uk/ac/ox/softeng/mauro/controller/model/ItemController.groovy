52f365d1ea1853120f46fa5993ed95056c8854d5
Joe Crawford <joseph.crawford@bdi.ox.ac.uk>
2024-03-05 10:22:50 +0000

    Add MetadataController

---
+++ mauro-api/src/main/groovy/uk/ac/ox/softeng/mauro/controller/model/ItemController.groovy
@@ -0,0 +1,69 @@
+package uk.ac.ox.softeng.mauro.controller.model
+
+import groovy.transform.CompileStatic
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.exceptions.HttpStatusException
+import uk.ac.ox.softeng.mauro.domain.model.Item
+
+@CompileStatic
+abstract class ItemController<I extends Item> {
+
+    /**
+     * Properties disallowed in a simple update request.
+     */
+    List<String> getDisallowedProperties() {
+        ['class', 'id', 'dateCreated', 'lastUpdated', 'domainType', 'createdBy']
+    }
+
+    /**
+     * Properties disallowed in a simple create request.
+     */
+    List<String> getDisallowedCreateProperties() {
+        disallowedProperties
+    }
+
+    protected I cleanBody(I item) {
+        I defaultItem = (I) item.class.getDeclaredConstructor().newInstance()
+
+        // Disallowed properties cannot be set by user request
+        disallowedCreateProperties.each {String key ->
+            if (defaultItem.hasProperty(key).properties.setter && item[key] != defaultItem[key]) {
+                throw new HttpStatusException(HttpStatus.BAD_REQUEST, "Property $key cannot be set directly")
+            }
+        }
+
+        // Collection properties cannot be set in user requests as these might be used in services
+        defaultItem.properties.each {
+            String key = it.key
+            if (defaultItem.hasProperty(key).properties.setter && (it.value instanceof Collection || it.value instanceof Map)) {
+                if (item[key]) throw new HttpStatusException(HttpStatus.BAD_REQUEST, "Collection or Map $key cannot be set directly")
+                item[key] = null
+            }
+        }
+
+        item
+    }
+
+    protected Item updateCreationProperties(Item item) {
+        item.id = null
+        item.version = null
+        item.dateCreated = null
+        item.lastUpdated = null
+        item.createdBy = 'USER@example.org'
+        item
+    }
+
+    protected boolean updateProperties(I existing, I cleanItem) {
+        boolean hasChanged
+        existing.properties.each {
+            String key = it.key
+            if (!disallowedProperties.contains(key) && cleanItem[key] != null && existing.hasProperty(key).properties.setter) {
+                if (existing[key] != cleanItem[key]) {
+                    existing[key] = cleanItem[key]
+                    hasChanged = true
+                }
+            }
+        }
+        return hasChanged
+    }
+}
