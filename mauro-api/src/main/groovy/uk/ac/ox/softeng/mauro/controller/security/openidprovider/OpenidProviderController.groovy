d92c5f9e51b15945b6de7683d213345968e140f9
Regina Hart <regina.hart@answerdigital.com>
2024-11-04 16:32:11 +0000

    adding wip openidconnectprovider controller for the get /openidConnectProviders endpoint

---
+++ mauro-api/src/main/groovy/uk/ac/ox/softeng/mauro/controller/security/openidprovider/OpenidProviderController.groovy
@@ -0,0 +1,58 @@
+package uk.ac.ox.softeng.mauro.controller.security.openidprovider
+
+
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+import io.micronaut.context.annotation.Value
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.security.annotation.Secured
+import io.micronaut.security.rules.SecurityRule
+import uk.ac.ox.softeng.mauro.web.ListResponse
+
+@CompileStatic
+@Slf4j
+@Controller('/openidConnectProviders')
+@Secured(SecurityRule.IS_ANONYMOUS)
+class OpenidProviderController {
+
+
+    @Value('${micronaut.security.openid-provider.id:b2092268-3014-4c24-a9cc-0913b2c63cfb}')
+    String openidProviderId
+
+    @Value('${micronaut.security.openid-provider.label:Keycloak}')
+    String label
+
+    @Value('${micronaut.security.openid-provider.standardProvider:true}')
+    boolean standardProvider
+
+    @Value('${micronaut.security.openid-provider.authorizationEndpoint:http://tvssnsdedm001.oxnet.nhs.uk:8095/realms/test/protocol/openid-connect/auth?scope=openid+email+profile&response_type=code&client_id=mdm&state=3b2e1535-5cae-49f7-83c3-0032aa7539c9&nonce=6ea24466-f8f4-3089-8881-e6b76eb70d5a}')
+    String authorizationEndpoint
+    @Value('${micronaut.security.openid-provider.imageUrl:https://upload.wikimedia.org/wikipedia/commons/2/29/Keycloak_Logo.png?20200311211229}')
+    String imageUrl
+
+    @Get()
+    ListResponse<OpenidConnectProvider> list() {
+        OpenidConnectProvider openidConnectProvider = new OpenidConnectProvider(openidProviderId, label, standardProvider, authorizationEndpoint,
+                imageUrl)
+        ListResponse.from(List.of(openidConnectProvider))
+    }
+
+
+    class OpenidConnectProvider {
+        UUID openidProviderId
+        String label
+        boolean standardProvider
+        String authorizationEndpoint
+        String imageUrl
+
+        OpenidConnectProvider(String openidProviderId, String label, boolean standardProvider,
+                              String authorizationEndpoint, String imageUrl) {
+            this.openidProviderId = UUID.fromString(openidProviderId)
+            this.label = label
+            this.standardProvider = standardProvider
+            this.authorizationEndpoint = authorizationEndpoint
+            this.imageUrl = imageUrl
+        }
+    }
+}
--- mauro-api/src/main/groovy/uk/ac/ox/softeng/mauro/security/authentication/MauroOpenIdAuthenticationMapper.groovy
+++ mauro-api/src/main/groovy/uk/ac/ox/softeng/mauro/security/authentication/MauroOpenIdAuthenticationMapper.groovy
@@ -2,6 +2,7 @@ package uk.ac.ox.softeng.mauro.security.authentication
 
 import groovy.util.logging.Slf4j
 import io.micronaut.context.annotation.Replaces
+import io.micronaut.context.annotation.Value
 import io.micronaut.security.authentication.AuthenticationException
 import io.micronaut.security.config.AuthenticationModeConfiguration
 import io.micronaut.security.oauth2.configuration.OpenIdAdditionalClaimsConfiguration
@@ -20,9 +21,11 @@ import uk.ac.ox.softeng.mauro.security.utils.SecureRandomStringGenerator
 @Replaces(DefaultOpenIdAuthenticationMapper)
 class MauroOpenIdAuthenticationMapper extends DefaultOpenIdAuthenticationMapper {
 
+    @Value('${micronaut.security.create-user:true}')
+    boolean createUser
+
     @Inject
     ItemCacheableRepository.CatalogueUserCacheableRepository catalogueUserCacheableRepository
-
 
     MauroOpenIdAuthenticationMapper(OpenIdAdditionalClaimsConfiguration openIdAdditionalClaimsConfiguration, AuthenticationModeConfiguration authenticationModeConfiguration) {
         super(openIdAdditionalClaimsConfiguration, authenticationModeConfiguration)
@@ -32,28 +35,36 @@ class MauroOpenIdAuthenticationMapper extends DefaultOpenIdAuthenticationMapper 
     @Transactional
     Map<String, Object> buildAttributes(String providerName, OpenIdTokenResponse tokenResponse, OpenIdClaims openIdClaims) {
         Map<String, Object> claims = super.buildAttributes(providerName, tokenResponse, openIdClaims)
-        if (!claims.email_verified) throw new AuthenticationException("Attempt to login with unverified email address! [${claims.email}]")
+        if (!claims.email_verified) authenticationException("Attempt to login with unverified email address! [${claims.email}]")
         CatalogueUser user = catalogueUserCacheableRepository.readByEmailAddress((String) claims.email)?: createUser(claims)
+        if (!user) authenticationException("User does not exist for $claims.email")
         claims.id = user.id
         claims
     }
 
     CatalogueUser createUser(Map<String, Object> claims) {
-        log.debug("User email address not found, adding new Catalogue user for : {}", claims.email)
-        CatalogueUser newUser = new CatalogueUser().tap {
-            pending = false
-            disabled = false
-            creationMethod = 'OPENID_REGISTER'
-            tempPassword = null
-            password = null
-            firstName = claims.given_name
-            lastName = claims.family_name
-            emailAddress = claims.email
-            salt = SecureRandomStringGenerator.generateSalt()
-        }
+        if (createUser) {
+            log.debug("User email address not found, adding new Catalogue user for : {}", claims.email)
+            CatalogueUser newUser = new CatalogueUser().tap {
+                pending = false
+                disabled = false
+                creationMethod = 'OPENID_REGISTER'
+                tempPassword = null
+                password = null
+                firstName = claims.given_name
+                lastName = claims.family_name
+                emailAddress = claims.email
+                salt = SecureRandomStringGenerator.generateSalt()
+            }
 
-        CatalogueUser saved = catalogueUserCacheableRepository.save(newUser)
-        catalogueUserCacheableRepository.invalidate(saved)
-        saved
+            CatalogueUser saved = catalogueUserCacheableRepository.save(newUser)
+            catalogueUserCacheableRepository.invalidate(saved)
+            saved
+        }
+        null
+    }
+
+    static void authenticationException(String message){
+        throw new AuthenticationException(message)
     }
 }
