866d9e9d6d4c20720283bcf08a34090133c0dbf1
Joe Crawford <joseph.crawford@bdi.ox.ac.uk>
2024-11-14 17:56:10 +0000

    Enable OpenID Connect login via UI

--- mauro-api/src/main/groovy/uk/ac/ox/softeng/mauro/controller/security/openidprovider/OpenidProviderController.groovy
+++ mauro-api/src/main/groovy/uk/ac/ox/softeng/mauro/controller/security/openidprovider/OpenidProviderController.groovy
@@ -1,6 +1,5 @@
 package uk.ac.ox.softeng.mauro.controller.security.openidprovider
 
-
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import io.micronaut.context.annotation.Value
@@ -8,7 +7,6 @@ import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.SecurityRule
-import uk.ac.ox.softeng.mauro.web.ListResponse
 
 @CompileStatic
 @Slf4j
@@ -17,18 +15,18 @@ import uk.ac.ox.softeng.mauro.web.ListResponse
 class OpenidProviderController {
 
 
-    @Value('${micronaut.security.openid-provider.id:b2092268-3014-4c24-a9cc-0913b2c63cfb}')
+    @Value('${micronaut.security.openid-provider.id}')
     String openidProviderId
 
-    @Value('${micronaut.security.openid-provider.label:Keycloak}')
+    @Value('${micronaut.security.openid-provider.label}')
     String label
 
-    @Value('${micronaut.security.openid-provider.standardProvider:true}')
+    @Value('${micronaut.security.openid-provider.standardProvider}')
     boolean standardProvider
 
-    @Value('${micronaut.security.openid-provider.authorizationEndpoint:http://tvssnsdedm001.oxnet.nhs.uk:8095/realms/test/protocol/openid-connect/auth?scope=openid+email+profile&response_type=code&client_id=mdm&state=3b2e1535-5cae-49f7-83c3-0032aa7539c9&nonce=6ea24466-f8f4-3089-8881-e6b76eb70d5a}')
+    @Value('${micronaut.security.openid-provider.authorizationEndpoint}')
     String authorizationEndpoint
-    @Value('${micronaut.security.openid-provider.imageUrl:https://upload.wikimedia.org/wikipedia/commons/2/29/Keycloak_Logo.png?20200311211229}')
+    @Value('${micronaut.security.openid-provider.imageUrl}')
     String imageUrl
 
     @Get()
