d4e568195631fe834fab6751402a836e8a312042
James Welch <james.welch@bdi.ox.ac.uk>
2024-11-29 09:34:45 +0000

    Implementation of API Keys for authentication and some lovely integration tests

--- mauro-api/src/test/groovy/uk/ac/ox/softeng/mauro/security/ApiKeyIntegrationSpec.groovy
+++ mauro-api/src/test/groovy/uk/ac/ox/softeng/mauro/security/ApiKeyIntegrationSpec.groovy
@@ -1,9 +1,12 @@
 package uk.ac.ox.softeng.mauro.security
 
+import uk.ac.ox.softeng.mauro.domain.email.Email
 import uk.ac.ox.softeng.mauro.domain.security.ApiKey
+import uk.ac.ox.softeng.mauro.domain.security.CatalogueUser
 import uk.ac.ox.softeng.mauro.persistence.SecuredContainerizedTest
 import uk.ac.ox.softeng.mauro.web.ListResponse
 
+import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.runtime.EmbeddedApplication
@@ -140,6 +143,208 @@ class ApiKeyIntegrationSpec extends SecuredIntegrationSpec {
         apiKeys.count == 0
 
 
+    }
+
+    void 'Test api key works for user'() {
+        given:
+        loginUser()
+
+        when:
+        ApiKey response = POST("/catalogueUsers/$user.id/apiKeys", [
+            name         : "Test Key",
+            expiresInDays: 10,
+            refreshable  : true
+        ], ApiKey)
+
+        String apiKeyId = response.id
+
+        logout()
+
+        // Check a couple of endpoints are unavailable after logging out
+
+        GET("admin/emails")
+        then:
+        def e = thrown(Exception)
+        e.message == "Unauthorized"
+
+        when:
+        GET("catalogueUsers/currentUser")
+
+        then:
+        e = thrown(Exception)
+        e.message == "User is not authenticated"
+
+        when:
+        apiKey = UUID.fromString(apiKeyId)
+        CatalogueUser userResponse = (CatalogueUser) GET("catalogueUsers/currentUser", CatalogueUser)
+
+        then:
+        userResponse.id == user.id
+
+        when: // But we still can't get the email list which requires an administrator
+        ListResponse<Email> emailsResponse = (ListResponse<Email>) GET("admin/emails", ListResponse, Email)
+
+        then:
+        e = thrown(Exception)
+        e.message == "Forbidden"
+
+        when: // when we remove the api key
+        apiKey = null
+        GET("catalogueUsers/currentUser")
+
+        then: // we're back to being unauthenticated
+        e = thrown(Exception)
+        e.message == "User is not authenticated"
+    }
+
+    void 'Test api key works for administrator'() {
+        given:
+        loginAdmin()
+
+        when:
+        ApiKey response = POST("/catalogueUsers/$adminUser.id/apiKeys", [
+            name         : "Test Key",
+            expiresInDays: 10,
+            refreshable  : true
+        ], ApiKey)
+
+        String apiKeyId = response.id
+
+        logout()
+
+        // Check a couple of endpoints are unavailable after logging out
+
+        GET("admin/emails")
+        then:
+        def e = thrown(Exception)
+        e.message == "Unauthorized"
+
+        when:
+        GET("catalogueUsers/currentUser")
+
+        then:
+        e = thrown(Exception)
+        e.message == "User is not authenticated"
+
+        when:
+        apiKey = UUID.fromString(apiKeyId)
+        CatalogueUser userResponse = (CatalogueUser) GET("catalogueUsers/currentUser", CatalogueUser)
+
+        then:
+        userResponse.id == adminUser.id
+
+        when: // But we still can't get the email list which requires an administrator
+        ListResponse<Email> emailsResponse = (ListResponse<Email>) GET("admin/emails", ListResponse, Email)
+
+        then:
+        emailsResponse.count == 0
+
+        when: // when we remove the api key
+        apiKey = null
+        GET("catalogueUsers/currentUser")
+
+        then: // we're back to being unauthenticated
+        e = thrown(Exception)
+        e.message == "User is not authenticated"
+    }
+
+
+    void 'Test random api key doesnt work'() {
+        given:
+        loginUser()
+
+        when:
+        ApiKey response = POST("/catalogueUsers/$user.id/apiKeys", [
+            name         : "Test Key",
+            expiresInDays: 10,
+            refreshable  : true
+        ], ApiKey)
+
+        String apiKeyId = response.id
+
+        logout()
+
+        // Check a couple of endpoints are unavailable after logging out
+
+        GET("catalogueUsers/currentUser")
+
+        then:
+        def e = thrown(Exception)
+        e.message == "User is not authenticated"
+
+        when:
+        apiKey = UUID.randomUUID()
+        CatalogueUser userResponse = (CatalogueUser) GET("catalogueUsers/currentUser", CatalogueUser)
+
+        then:
+        e = thrown(Exception)
+        e.message == "User is not authenticated"
+    }
+
+    void 'Test disabled api key doesnt work'() {
+        given:
+        loginUser()
+
+        when:
+        ApiKey response = POST("/catalogueUsers/$user.id/apiKeys", [
+            name         : "Test Key",
+            expiresInDays: 10,
+            refreshable  : true,
+            disabled     : true
+        ], ApiKey)
+
+        String apiKeyId = response.id
+
+        logout()
+
+        // Check a couple of endpoints are unavailable after logging out
+
+        GET("catalogueUsers/currentUser")
+
+        then:
+        def e = thrown(Exception)
+        e.message == "User is not authenticated"
+
+        when:
+        apiKey = UUID.randomUUID()
+        CatalogueUser userResponse = (CatalogueUser) GET("catalogueUsers/currentUser", CatalogueUser)
+
+        then:
+        e = thrown(Exception)
+        e.message == "User is not authenticated"
+    }
+
+    void 'Test expired api key doesnt work'() {
+        given:
+        loginUser()
+
+        when:
+        ApiKey response = POST("/catalogueUsers/$user.id/apiKeys", [
+            name         : "Test Key",
+            expiresInDays: -1,
+            refreshable  : true,
+            disabled     : true
+        ], ApiKey)
+
+        String apiKeyId = response.id
+
+        logout()
+
+        // Check a couple of endpoints are unavailable after logging out
+
+        GET("catalogueUsers/currentUser")
+
+        then:
+        def e = thrown(Exception)
+        e.message == "User is not authenticated"
+
+        when:
+        apiKey = UUID.randomUUID()
+        CatalogueUser userResponse = (CatalogueUser) GET("catalogueUsers/currentUser", CatalogueUser)
+
+        then:
+        e = thrown(Exception)
+        e.message == "User is not authenticated"
     }
 
 }
