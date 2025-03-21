b4138b0c3262d6ebe9787a359f6afd6cc0457963
Joe Crawford <joseph.crawford@bdi.ox.ac.uk>
2025-03-21 16:40:53 +0000

    WIP get query builder page to open in mdm-explorer

--- mauro-api/src/main/groovy/uk/ac/ox/softeng/mauro/controller/datamodel/DataModelController.groovy
+++ mauro-api/src/main/groovy/uk/ac/ox/softeng/mauro/controller/datamodel/DataModelController.groovy
@@ -32,7 +32,6 @@ import uk.ac.ox.softeng.mauro.domain.model.version.CreateNewVersionData
 import uk.ac.ox.softeng.mauro.domain.model.version.FinaliseData
 import uk.ac.ox.softeng.mauro.domain.model.version.ModelVersion
 import uk.ac.ox.softeng.mauro.domain.security.Role
-import uk.ac.ox.softeng.mauro.persistence.cache.AdministeredItemCacheableRepository
 import uk.ac.ox.softeng.mauro.persistence.cache.AdministeredItemCacheableRepository.DataElementCacheableRepository
 import uk.ac.ox.softeng.mauro.persistence.cache.AdministeredItemCacheableRepository.DataTypeCacheableRepository
 import uk.ac.ox.softeng.mauro.persistence.cache.ModelCacheableRepository.DataModelCacheableRepository
