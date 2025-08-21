9ca3ac0ce93a76f884d26c8e8761a0d146e38211
James Welch <jamesrwelch@gmail.com>
2025-08-21 13:26:11 +0100

    Add aliases to profile fields so they work better with the JSON format

--- mauro-domain/src/main/groovy/org/maurodata/profile/ProfileSection.groovy
+++ mauro-domain/src/main/groovy/org/maurodata/profile/ProfileSection.groovy
@@ -1,5 +1,6 @@
 package org.maurodata.profile
 
+import com.fasterxml.jackson.annotation.JsonAlias
 import groovy.transform.CompileStatic
 import io.micronaut.core.annotation.Introspected
 
@@ -7,7 +8,10 @@ import io.micronaut.core.annotation.Introspected
 @CompileStatic
 class ProfileSection {
 
+    @JsonAlias('sectionName')
     String label
+
+    @JsonAlias('sectionDescription')
     String description
 
     List<ProfileField> fields
