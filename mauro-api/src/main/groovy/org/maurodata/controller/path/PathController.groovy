f801792a8bdf9ffa525f970f83724784d6354a1a
Regina Hart <regina.hart@answerdigital.com>
2025-09-22 16:28:06 +0100

    adding methods in pathsservice to wrap the controller endpoints

--- mauro-api/src/main/groovy/org/maurodata/controller/path/PathController.groovy
+++ mauro-api/src/main/groovy/org/maurodata/controller/path/PathController.groovy
@@ -2,21 +2,18 @@ package org.maurodata.controller.path
 
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
-import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.SecurityRule
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
-import org.maurodata.ErrorHandler
 import org.maurodata.api.Paths
 import org.maurodata.api.path.PathApi
 import org.maurodata.audit.Audit
 import org.maurodata.controller.model.ItemController
 import org.maurodata.domain.model.AdministeredItem
 import org.maurodata.domain.model.Item
-import org.maurodata.domain.security.Role
 import org.maurodata.persistence.model.ItemRepository
 import org.maurodata.persistence.model.PathRepository
 import org.maurodata.service.path.PathService
@@ -35,39 +32,19 @@ class PathController<I extends Item> extends ItemController<I> implements PathAp
         super(pathRepository as ItemRepository<I>)
     }
 
-
     @Audit
     @Override
     @Get(Paths.RESOURCE_BY_PATH)
     AdministeredItem getResourceByPath(String domainType, String path) {
-        AdministeredItem item = pathService.findResourceByPath(domainType, path)
-        ErrorHandler.handleErrorOnNullObject(HttpStatus.NOT_FOUND, item, "Item with DomainType $domainType not found with path: $path")
-
-        accessControlService.checkRole(Role.READER, item)
-        pathService.updateDerivedProperties(item)
-        item
+        pathService.getResourceByPath(domainType, path)
     }
 
     @Audit
     @Override
     @Get(Paths.RESOURCE_BY_PATH_FROM_RESOURCE)
     AdministeredItem getResourceByPathFromResource(String domainType, UUID domainId, String path) {
-        AdministeredItem fromModel = findAdministeredItem(domainType, domainId)
-        ErrorHandler.handleErrorOnNullObject(HttpStatus.NOT_FOUND, fromModel, "Model $domainType, $domainId not found")
+        pathService.getResourceByPathFromResource(domainType, domainId, path)
 
-        accessControlService.checkRole(Role.READER, fromModel)
-        pathService.updateDerivedProperties(fromModel)
-
-        //verify model path in the input path
-        if (!path.contains(fromModel.path.pathString)) {
-            ErrorHandler.handleError(HttpStatus.NOT_FOUND, "Path $path does not belong to $domainType, $domainId")
-        }
-        Tuple2<String, String> itemDomainTypeAndPath = pathService.getItemDomainTypeAndPath(path)
-        AdministeredItem administeredItem = pathService.findResourceByPath(itemDomainTypeAndPath.first, path)
-        ErrorHandler.handleErrorOnNullObject(HttpStatus.NOT_FOUND, administeredItem, "Item with $itemDomainTypeAndPath.first  and label $itemDomainTypeAndPath.v2 not found")
-        accessControlService.checkRole(Role.READER, administeredItem)
-        pathService.updateDerivedProperties(administeredItem as I)
-        administeredItem
     }
 
 }
