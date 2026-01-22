0da73760d0a524914fe8282f1c7291ffe669319e
James Welch <james.welch@bdi.ox.ac.uk>
2026-01-22 14:49:11 +0000

    Fix the "secure" part of the cookie based on the host scheme

--- mauro-api/src/main/groovy/org/maurodata/security/authentication/OAuthRedirectFilter.groovy
+++ mauro-api/src/main/groovy/org/maurodata/security/authentication/OAuthRedirectFilter.groovy
@@ -8,6 +8,8 @@ import io.micronaut.http.cookie.Cookie
 import io.micronaut.http.cookie.SameSite
 import io.micronaut.http.filter.HttpServerFilter
 import io.micronaut.http.filter.ServerFilterChain
+import io.micronaut.http.server.util.HttpHostResolver
+import jakarta.inject.Inject
 import org.reactivestreams.Publisher
 import reactor.core.publisher.Mono
 
@@ -17,16 +19,24 @@ class OAuthRedirectFilter implements HttpServerFilter {
 
     static final String UI_REDIRECT_URL = "ui_redirect_url"
 
+    @Inject HttpHostResolver httpHostResolver
+
+
     @Override
     Publisher<MutableHttpResponse<?>> doFilter(HttpRequest<?> request, ServerFilterChain chain ) { // (1)
         String redirectUri = null
         redirectUri = request.getParameters().get("redirect_uri")
+
+        String host = httpHostResolver.resolve(request)
+        URI requestHostURI = new URI(host)
+        requestHostURI.scheme
+
         // Use Mono.from to turn publisher -> Mono, then map the response
         return Mono.from(chain.proceed(request))
             .map { MutableHttpResponse<?> resp ->
                 if(redirectUri) {
                     Cookie cookie = Cookie.of(UI_REDIRECT_URL, redirectUri)
-                        .secure(false) // For local dev remove secure; in prod keep it
+                        .secure(requestHostURI.scheme == "https")
                         .sameSite(SameSite.None)
                         .httpOnly(true)
                         .path("/")
