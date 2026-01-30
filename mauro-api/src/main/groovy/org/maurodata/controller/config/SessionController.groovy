49f6f48493c47adb5dc084818d354def5fcdea67
Joe Crawford <joseph.crawford@bdi.ox.ac.uk>
2026-01-30 13:38:13 +0000

    gh-314 Restrict activeSessions endpoint to admin + Remove session id from response + Reformat

--- mauro-api/src/main/groovy/org/maurodata/controller/config/SessionController.groovy
+++ mauro-api/src/main/groovy/org/maurodata/controller/config/SessionController.groovy
@@ -7,6 +7,7 @@ import org.maurodata.controller.security.tracking.SessionTracker
 import org.maurodata.controller.security.tracking.TrackedSession
 import org.maurodata.domain.security.CatalogueUser
 import org.maurodata.persistence.security.CatalogueUserRepository
+import org.maurodata.security.AccessControlService
 
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
@@ -17,7 +18,6 @@ import io.micronaut.security.annotation.Secured
 import io.micronaut.security.authentication.Authentication
 import io.micronaut.security.rules.SecurityRule
 import jakarta.inject.Inject
-import org.maurodata.security.AccessControlService
 
 @CompileStatic
 @Slf4j
@@ -76,45 +76,46 @@ class SessionController implements SessionApi {
     @Override
     @Get(Paths.SESSION_ADMIN_ACTIVE_SESSIONS)
     Map activeSessions() {
+        accessControlService.checkAdministrator()
+
         Collection<TrackedSession> trackedSessions = sessionTracker.activeSessions
 
-        List<Map> items=[]
-        List<Map> authorisedItems=[]
-        List<Map> unauthorisedItems=[]
+        List<Map> items = []
+        List<Map> authorisedItems = []
+        List<Map> unauthorisedItems = []
 
-        trackedSessions.forEach { TrackedSession trackedSession ->
+        trackedSessions.forEach {TrackedSession trackedSession ->
 
             CatalogueUser catalogueUser = null
 
-            if(trackedSession.userEmailAddress != null && !trackedSession.userEmailAddress.isEmpty() && trackedSession.userEmailAddress != TrackedSession.UNLOGGED_USER_EMAIL) {
+            if (trackedSession.userEmailAddress != null && !trackedSession.userEmailAddress.isEmpty() && trackedSession.userEmailAddress !=
+                TrackedSession.UNLOGGED_USER_EMAIL) {
                 catalogueUser = catalogueUserRepository.readByEmailAddress(trackedSession.userEmailAddress)
             }
 
             LinkedHashMap item = [
-                id: trackedSession.id,
-                lastAccessedDateTime : trackedSession.lastAccessedDateTime,
-                creationDateTime : trackedSession.creationDateTime,
-                userEmailAddress : trackedSession.userEmailAddress?trackedSession.userEmailAddress:TrackedSession.UNLOGGED_USER_EMAIL,
-                userName: catalogueUser?catalogueUser.fullName:null,
-                userOrganisation: catalogueUser?catalogueUser.organisation:null,
-                lastAccessedUrl: trackedSession.lastAccessedUrl
+                lastAccessedDateTime: trackedSession.lastAccessedDateTime,
+                creationDateTime    : trackedSession.creationDateTime,
+                userEmailAddress    : trackedSession.userEmailAddress ? trackedSession.userEmailAddress : TrackedSession.UNLOGGED_USER_EMAIL,
+                userName            : catalogueUser ? catalogueUser.fullName : null,
+                userOrganisation    : catalogueUser ? catalogueUser.organisation : null,
+                lastAccessedUrl     : trackedSession.lastAccessedUrl
             ]
 
             items.add(item)
-            if(catalogueUser!=null){
+            if (catalogueUser != null) {
                 authorisedItems.add(item)
             } else {
                 unauthorisedItems.add(item)
             }
         }
 
-        Map<String,Object> activeSessions = [
-
-            countAuthorised: authorisedItems.size(),
+        Map<String, Object> activeSessions = [
+            countAuthorised  : authorisedItems.size(),
             countUnauthorised: unauthorisedItems.size(),
-            items: items,
-            authorisedItems: authorisedItems,
-            unauthorisedItems : unauthorisedItems
+            items            : items,
+            authorisedItems  : authorisedItems,
+            unauthorisedItems: unauthorisedItems
         ]
 
         return activeSessions
