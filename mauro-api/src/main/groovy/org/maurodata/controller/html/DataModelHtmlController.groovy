1bb622b69a89e8898b79ba437eb1c0c1edc468c6
Joe Crawford <joseph.crawford@bdi.ox.ac.uk>
2026-02-04 18:13:42 +0000

    add sibling tables

--- mauro-api/src/main/groovy/org/maurodata/controller/html/DataModelHtmlController.groovy
+++ mauro-api/src/main/groovy/org/maurodata/controller/html/DataModelHtmlController.groovy
@@ -1,11 +1,5 @@
 package org.maurodata.controller.html
 
-import org.maurodata.controller.datamodel.DataModelController
-import org.maurodata.domain.datamodel.DataClass
-import org.maurodata.domain.datamodel.DataModel
-import org.maurodata.persistence.cache.AdministeredItemCacheableRepository
-
-import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
@@ -15,10 +9,18 @@ import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.SecurityRule
 import io.micronaut.views.View
 import jakarta.inject.Inject
+import org.maurodata.controller.datamodel.DataClassController
+import org.maurodata.controller.datamodel.DataModelController
+import org.maurodata.domain.datamodel.DataClass
+import org.maurodata.domain.datamodel.DataModel
+import org.maurodata.persistence.cache.AdministeredItemCacheableRepository
+import org.maurodata.persistence.cache.AdministeredItemCacheableRepository.DataElementCacheableRepository
+import org.maurodata.persistence.cache.AdministeredItemCacheableRepository.DataTypeCacheableRepository
+import org.maurodata.persistence.model.PathRepository
 
 @Slf4j
 @Controller
-@CompileStatic
+//@CompileStatic
 @Secured(SecurityRule.IS_ANONYMOUS)
 class DataModelHtmlController {
 
@@ -26,7 +28,19 @@ class DataModelHtmlController {
     DataModelController dataModelController
 
     @Inject
+    DataClassController dataClassController
+
+    @Inject
     AdministeredItemCacheableRepository.DataClassCacheableRepository dataClassRepository
+
+    @Inject
+    DataElementCacheableRepository dataElementCacheableRepository
+
+    @Inject
+    DataTypeCacheableRepository dataTypeCacheableRepository
+
+    @Inject
+    PathRepository pathRepository
 
     @Produces(MediaType.TEXT_HTML)
     @View('dataModel.html')
@@ -41,5 +55,47 @@ class DataModelHtmlController {
         }
 
         dataModel
+    }
+
+    @Produces(MediaType.TEXT_HTML)
+    @View('content-browser.html')
+    @Get('/html/browser/dataClass/{id}')
+    Map<String, Object> showDataClass(UUID id) {
+        DataClass dataClass = dataClassController.show(id)
+
+        dataClass.dataElements = dataElementCacheableRepository.findAllByDataClass(dataClass)
+
+        dataClass.dataElements.each {
+            it.dataType = dataTypeCacheableRepository.findById(it.dataType.id)
+        }
+
+        String rowCount = dataClass.metadata.find {it.namespace == 'org.maurodata.plugin.sql.database.sqlserver' && it.key == 'row_count'}.value
+
+        pathRepository.readParentItems(dataClass)
+
+        DataClass schemaClass = dataClass.parentDataClass
+
+        List<DataClass> siblingTables = dataClassRepository.readAllByParentDataClass(schemaClass).sort {it.label}
+
+        [
+            label: dataClass.label,
+            rowCount: rowCount,
+            dataElements: dataClass.dataElements.collect {
+                [
+                    label: it.label,
+                    description: it.description,
+                    dataTypeLabel: it.metadata.find {it.namespace == 'org.maurodata.plugin.sql.database.sqlserver' && it.key == 'data_type'}?.value,
+                    rowCount: it.metadata.find {it.namespace == 'uk.ac.ox.softeng.maurodatamapper.plugins.explorer.research' && it.key == 'rowCount'}?.value,
+                    distinctValuesCount: it.metadata.find {it.namespace == 'org.maurodata.plugin.sql.database.sqlserver' && it.key == 'distinct_values_count'}?.value,
+                    nonNullValuesCount: it.metadata.find {it.namespace == 'uk.ac.ox.softeng.maurodatamapper.plugins.explorer.research' && it.key == 'notNullValuesCount'}?.value
+                ]
+            },
+            tables: siblingTables.collect {
+                [
+                    label: it.label,
+                    active: it.label == dataClass.label
+                ]
+            }
+        ]
     }
 }
