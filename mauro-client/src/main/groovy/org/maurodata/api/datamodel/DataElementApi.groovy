0deb8bf764a0964c5f970ab852676b41445239da
James Welch <james.welch@bdi.ox.ac.uk>
2026-02-12 11:50:46 +0000

    Fix failing test

--- mauro-client/src/main/groovy/org/maurodata/api/datamodel/DataElementApi.groovy
+++ mauro-client/src/main/groovy/org/maurodata/api/datamodel/DataElementApi.groovy
@@ -41,6 +41,9 @@ interface DataElementApi extends AdministeredItemApi<DataElement, DataClass> {
     @Get(Paths.DATA_ELEMENT_IN_MODEL_LIST)
     ListResponse<DataElement> byModelList(UUID dataModelId, @Nullable PaginationParams params)
 
+    @Get(Paths.DATA_ELEMENT_IN_MODEL_LIST)
+    ListResponse<DataElement> byModelList(UUID dataModelId)
+
     @Post(Paths.DATA_ELEMENT_COPY)
     DataElement copyDataElement(UUID dataModelId, UUID dataClassId, UUID otherModelId,  UUID otherDataClassId, UUID dataElementId)
 
