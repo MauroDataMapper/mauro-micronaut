33e6e495954aa3370f0305c1d43e41e92240b370
James Welch <jamesrwelch@gmail.com>
2026-04-07 16:13:45 +0100

    Fix for when folder / versioned folder cache is not invalidated

--- mauro-persistence/src/main/groovy/org/maurodata/persistence/cache/FacetCacheableRepository.groovy
+++ mauro-persistence/src/main/groovy/org/maurodata/persistence/cache/FacetCacheableRepository.groovy
@@ -52,6 +52,16 @@ abstract class FacetCacheableRepository<F extends Facet> extends ItemCacheableRe
         // invalidate find of the parent item
         invalidateCachedLookupById(FIND_BY_ID, item.multiFacetAwareItemDomainType, item.multiFacetAwareItemId)
 
+        if(item.multiFacetAwareItemDomainType == 'VersionedFolder') {
+            // invalidate find of the parent item for Folder as well
+            invalidateCachedLookupById(FIND_BY_ID, 'Folder', item.multiFacetAwareItemId)
+        }
+
+        if(item.multiFacetAwareItemDomainType == 'Folder') {
+            // invalidate find of the parent item for Folder as well
+            invalidateCachedLookupById(FIND_BY_ID, 'VersionedFolder', item.multiFacetAwareItemId)
+        }
+
         // invalidate findAll of the parent collection
         AdministeredItemCacheableRepository<AdministeredItem> administeredItemAdministeredItemCacheableRepository = getRepository(item.multiFacetAwareItemDomainType)
 
