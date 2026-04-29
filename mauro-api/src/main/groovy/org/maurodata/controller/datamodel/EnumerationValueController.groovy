712e65981d587450417589fae758cb0f562463ef
edcrichton <edwardcrichton@hotmail.com>
2026-04-29 11:56:02 +0100

    AccessControl for listing enumeration values should be READER not EDITOR

--- mauro-api/src/main/groovy/org/maurodata/controller/datamodel/EnumerationValueController.groovy
+++ mauro-api/src/main/groovy/org/maurodata/controller/datamodel/EnumerationValueController.groovy
@@ -83,7 +83,7 @@ class EnumerationValueController extends AdministeredItemController<EnumerationV
     ListResponse<EnumerationValue> list(UUID dataModelId, UUID enumerationTypeId, @Nullable PaginationParams params = new PaginationParams()) {
         
         DataType enumerationType = dataTypeRepository.readById(enumerationTypeId)
-        accessControlService.checkRole(Role.EDITOR, enumerationType)
+        accessControlService.checkRole(Role.READER, enumerationType)
         ListResponse.from(enumerationValueRepository.readAllByEnumerationType_Id(enumerationTypeId), params)
     }
 
