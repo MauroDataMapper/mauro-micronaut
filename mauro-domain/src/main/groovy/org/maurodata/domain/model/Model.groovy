d93081988acfa4654d32c6ed2b058d03025e9226
James Welch <james.welch@bdi.ox.ac.uk>
2026-07-27 12:30:05 +0100

    Properly set the model version tag.  Fixes #391

--- mauro-domain/src/main/groovy/org/maurodata/domain/model/Model.groovy
+++ mauro-domain/src/main/groovy/org/maurodata/domain/model/Model.groovy
@@ -352,7 +352,7 @@ abstract class Model extends AdministeredItem implements DiffableItem, Securable
         intoModel.folder = ItemUtils.copyItem(this.folder, intoModel.folder)
         intoModel.authority = ItemUtils.copyItem(this.authority, intoModel.authority)
         intoModel.modelVersion = ItemUtils.copyItem(this.modelVersion, intoModel.modelVersion)
-        intoModel.modelVersionTag = ItemUtils.copyItem(this.modelVersion, intoModel.modelVersion)
+        intoModel.modelVersionTag = ItemUtils.copyItem(this.modelVersionTag, intoModel.modelVersionTag)
         intoModel.versionLinks = ItemUtils.copyItems(this.versionLinks, intoModel.versionLinks)
     }
 }
